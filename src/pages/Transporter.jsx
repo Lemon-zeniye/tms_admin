d9bdf45e1fb61d82a727de71bdf77987193d780b
Robel <robelas2001@gmail.com>
2024-10-24 09:59:34 +0300

    transporter

--- src/pages/Transporter.jsx
+++ src/pages/Transporter.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import { DataTable } from "mantine-datatable";
 import { Box, Button, Flex, Pagination } from "@mantine/core";
@@ -11,32 +12,95 @@ function Transporter() {
     navigate("/addTransporter");
   };
 
-  const companies = [
-    {
-      id: "1",
-      company_name: "Transport Co.",
-      operational_area: "North America",
-      fleet_size: 50,
-      contact_person: {
-        name: "John Doe",
-        email: "john.doe@example.com",
-        phone_number: "+1234567890",
-      },
-      status: "Active",
-    },
-    {
-      id: "2",
-      company_name: "Logistics Solutions",
-      operational_area: "Europe",
-      fleet_size: 100,
-      contact_person: {
-        name: "Jane Smith",
-        email: "jane.smith@example.com",
-        phone_number: "+9876543210",
-      },
-      status: "Inactive",
-    },
-  ];
+  const location = useLocation();
+  const { tableData } = location.state || { tableData: [] };
+
+  // const companies = [
+  //   {
+  //     id: "1",
+  //     company_name: "Transport Co.",
+  //     operational_area: "North America",
+  //     fleet_size: 50,
+  //     contact_person: {
+  //       name: "John Doe",
+  //       email: "john.doe@example.com",
+  //       phone_number: "+1234567890",
+  //     },
+  //     status: "Active",
+  //   },
+  //   {
+  //     id: "2",
+  //     company_name: "Logistics Solutions",
+  //     operational_area: "Europe",
+  //     fleet_size: 100,
+  //     contact_person: {
+  //       name: "Jane Smith",
+  //       email: "jane.smith@example.com",
+  //       phone_number: "+9876543210",
+  //     },
+  //     status: "Inactive",
+  //   },
+  //   {
+  //     id: "3",
+  //     company_name: "Logistics Solutions",
+  //     operational_area: "Europe",
+  //     fleet_size: 200,
+  //     contact_person: {
+  //       name: "Jane Smith",
+  //       email: "jane.smith@example.com",
+  //       phone_number: "+9876543210",
+  //     },
+  //     status: "Inactive",
+  //   },
+  //   {
+  //     id: "4",
+  //     company_name: "Logistics Solutions",
+  //     operational_area: "Europe",
+  //     fleet_size: 10,
+  //     contact_person: {
+  //       name: "Jane Smith",
+  //       email: "jane.smith@example.com",
+  //       phone_number: "+9876543210",
+  //     },
+  //     status: "Inactive",
+  //   },
+  //   {
+  //     id: "5",
+  //     company_name: "Logistics Solutions",
+  //     operational_area: "Europe",
+  //     fleet_size: 10,
+  //     contact_person: {
+  //       name: "Jane Smith",
+  //       email: "jane.smith@example.com",
+  //       phone_number: "+9876543210",
+  //     },
+  //     status: "Inactive",
+  //   },
+  //   {
+  //     id: "6",
+  //     company_name: "Logistics Solutions",
+  //     operational_area: "Europe",
+  //     fleet_size: 10,
+  //     contact_person: {
+  //       name: "Jane Smith",
+  //       email: "jane.smith@example.com",
+  //       phone_number: "+9876543210",
+  //     },
+  //     status: "Inactive",
+  //   },
+  //   {
+  //     id: "7",
+  //     company_name: "Logistics Solutions",
+  //     operational_area: "Europe",
+  //     fleet_size: 10,
+  //     contact_person: {
+  //       name: "Jane Smith",
+  //       email: "jane.smith@example.com",
+  //       phone_number: "+9876543210",
+  //     },
+  //     status: "Inactive",
+  //   },
+  // ];
 
   const [activePage, setPage] = useState(1);
 
@@ -50,36 +114,21 @@ function Transporter() {
         borderRadius="md"
         withTableBorder
         columns={[
-          { accessor: "company_name", title: "Company Name" },
-          { accessor: "operational_area", title: "Operational Area" },
-          { accessor: "fleet_size", title: "Fleet Size" },
-          {
-            accessor: "contact_person",
-            title: "Contact Person",
-            render: (records) => (
-              <>
-                <Box>{records.contact_person.name}</Box>
-              </>
-            ),
-          },
-          {
-            accessor: "contact_phone_email",
-            title: "Contact Phone/Email",
-            render: (records) => (
-              <>
-                <Flex direction="column">
-                  <Box>{records.contact_person.email}</Box>
-                  <Box>{records.contact_person.phone_number}</Box>
-                </Flex>
-              </>
-            ),
-          },
-
-          { accessor: "status" },
+          { accessor: "companyName", title: "Company Name" },
+          { accessor: "operationalArea", title: "Operational Area" },
+          { accessor: "fleetSize", title: "Fleet Size" },
+          { accessor: "contactPersonName", title: "Contact Name" },
+          { accessor: "contactPersonEmail", title: "Contact Email" },
+          // { accessor: "status" },
         ]}
-        records={companies}
+        records={tableData}
       />
-      <Pagination value={activePage} onChange={setPage} total={10} className="float-right mt-10"/>
+      <Pagination
+        value={activePage}
+        onChange={setPage}
+        total={10}
+        className="float-right mt-10"
+      />
     </div>
   );
 }
