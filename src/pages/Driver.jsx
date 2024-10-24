1e6383a6150db7baeb5eb73ae2a5e28be31ee44e
mahlet <mahlet@gmail.com>
2024-10-24 10:51:29 +0300

    Driver

--- src/pages/Driver.jsx
+++ src/pages/Driver.jsx
@@ -1,82 +1,67 @@
-import React from "react";
+// import React from "react";
 import { DataTable } from "mantine-datatable";
 import { Box, Button } from "@mantine/core";
-import { TextInput, Flex, NumberInput } from "@mantine/core";
+import { TextInput, Flex, NumberInput, Container } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
-
+import React, { useState } from "react";
 function Driver() {
-  const companies = [
-    {
-      id: "1",
-      company_name: "Transport Co.",
-      operational_area: "North America",
-      fleet_size: 50,
-      contact_person: {
-        name: "John Doe",
-        email: "john.doe@example.com",
-        phone_number: "+1234567890",
-      },
-      status: "Active",
-    },
-    {
-      id: "2",
-      company_name: "Logistics Solutions",
-      operational_area: "Europe",
-      fleet_size: 100,
-      contact_person: {
-        name: "Jane Smith",
-        email: "jane.smith@example.com",
-        phone_number: "+9876543210",
-      },
-      status: "Inactive",
-    },
-    {
-      id: "3",
-      company_name: "Transport Co2.",
-      operational_area: "North America",
-      fleet_size: 50,
-      contact_person: {
-        name: "John Doe",
-        email: "john.doe@example.com",
-        phone_number: "+1234567890",
-      },
-      status: "Active",
-    },
-    {
-      id: "4",
-      company_name: "Transport Co.",
-      operational_area: "North America",
-      fleet_size: 50,
-      contact_person: {
-        name: "John Doe",
-        email: "john.doe@example.com",
-        phone_number: "+1234567890",
-      },
-      status: "Active",
-    },
-  ];
+  const [driverName, setDriverName] = useState("");
+  const [driverLicenseNumber, setLicenseNumber] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState(0);
+  const [licenceClassType, setLicenceClassType] = useState(0);
+  // const [experience(Year),  setExperience(Year)] = useState("");
+  // const [] = useState("");
+  const [tableData, setTableData] = useState([]);
 
+  const handleAddData = () => {
+    if (driverName && driverLicenseNumber && phoneNumber && licenceClassType) {
+      const newRow = {
+        id: tableData.length + 1,
+        driverName,
+        driverLicenseNumber,
+        phoneNumber,
+        licenceClassType,
+      };
+      setTableData([...tableData, newRow]);
+    }
+  };
   return (
     <div>
       {/* <Flex className="mb-4">
-        <Button>New Driver</Button>
-      </Flex> */}
+          <Button>New Driver</Button>
+        </Flex> */}
 
       <h2>Driver Information</h2>
 
       <Flex className="gap-20 mb-10">
-        <TextInput label="Driver Name" required />
-        <TextInput label="Driver License Number" required></TextInput>
+        <TextInput
+          label="Driver Name"
+          required
+          onChange={(e) => setDriverName(e.target.value)}
+        />
+        <TextInput
+          label="Driver License Number"
+          required
+          onChange={(e) => setLicenseNumber(e.target.value)}
+        />
         <DateInput
           valueFormat="YYYY MMM DD"
           label="Licence Expiry Date"
           placeholder="Date input"
           required
         />
-        <TextInput label="Licence Class/Type" required></TextInput>
+        <TextInput
+          label="Licence Class/Type"
+          required
+          onChange={(e) => setLicenceClassType(e.target.value)}
+        ></TextInput>
       </Flex>
       <Flex className="gap-20">
-        <TextInput label="Phone Number" required />
+        <TextInput
+          label="Phone Number"
+          required
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
         <TextInput label="Email Address" required></TextInput>
         <NumberInput label="Experience(Year)" placeholder="Input placeholder" />
         <TextInput label="Address" required></TextInput>
@@ -85,7 +70,7 @@ function Driver() {
         <Button variant="filled" color="red">
           CANCEL
         </Button>
-        <Button variant="filled" color="teal">
+        <Button variant="filled" color="teal" onClick={handleAddData}>
           SAVE
         </Button>
       </Flex>
@@ -95,34 +80,36 @@ function Driver() {
         borderRadius="md"
         withTableBorder
         columns={[
-          { accessor: "company_name", title: "Company Name" },
-          { accessor: "operational_area", title: "Operational Area" },
-          { accessor: "fleet_size", title: "Fleet Size" },
-          {
-            accessor: "contact_person",
-            title: "Contact Person",
-            render: (records) => (
-              <>
-                <Box>{records.contact_person.name}</Box>
-              </>
-            ),
-          },
-          {
-            accessor: "contact_phone_email",
-            title: "Contact Phone/Email",
-            render: (records) => (
-              <>
-                <Flex direction="column">
-                  <Box>{records.contact_person.email}</Box>
-                  <Box>{records.contact_person.phone_number}</Box>
-                </Flex>
-              </>
-            ),
-          },
+          { accessor: "driverName", title: "Driver Name" },
+          { accessor: "driverLicenseNumber", title: "License Number" },
+          { accessor: "phoneNumber", title: "Phone Number" },
+          { accessor: "licenceClassType", title: "Licence Class" },
 
-          { accessor: "status" },
+          // {
+          //   accessor: "contact_person",
+          //   title: "Contact Person",
+          //   render: (records) => (
+          //     <>
+          //       <Box>{records.contact_person.name}</Box>
+          //     </>
+          //   ),
+          // },
+          // {
+          //   accessor: "contact_phone_email",
+          //   title: "Contact Phone/Email",
+          //   render: (records) => (
+          //     <>
+          //       <Flex direction="column">
+          //         <Box>{records.contact_person.email}</Box>
+          //         <Box>{records.contact_person.phone_number}</Box>
+          //       </Flex>
+          //     </>
+          //   ),
+          // },
+
+          // { accessor: "status" },
         ]}
-        records={companies}
+        records={tableData}
       />
     </div>
   );
